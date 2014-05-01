e93c85ca1851d7b9487968c60b67b4a689843e08
NiL-22 <nazareno.levy@gmail.com>
2014-04-30 21:21:52 -0300

    Chau business y common

---
+++ src/FrbaCommerce/Clases/ConexionBD.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace FrbaCommerce.Common
+{
+    public class ConexionBD
+    {
+        public static string String_Conexion = "Server=localhost\\SQLSERVER2008;Database=GD1C2014;User Id=gd;Password=gd2014;";
+    
+         
+        SqlConnection _con = new SqlConnection(String_Conexion);
+        
+  
+
+       try{
+            _con.Open();
+            
+        
+
+
+        _con.Close();
+
+   }
+}
+      
