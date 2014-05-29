5428878fdd14602abc98ce1b0af483e3a9c32829
castellojulian89 <castellojulian@gmail.com>
2014-05-29 00:05:53 -0300

    Arrancado Comprar/Ofertar con algunos test succes!

--- src/FrbaCommerce/Comprar-Ofertar/ComprarOfertar.Designer.cs
+++ src/FrbaCommerce/Comprar-Ofertar/ComprarOfertar.Designer.cs
@@ -28,11 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 85);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(873, 396);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(109, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(233, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(397, 60);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // ComprarOfertar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 493);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ComprarOfertar";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
--- src/FrbaCommerce/Comprar-Ofertar/ComprarOfertar.cs
+++ src/FrbaCommerce/Comprar-Ofertar/ComprarOfertar.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using FrbaCommerce.Common;
+using System.Data.SqlClient;
 
 namespace FrbaCommerce.Comprar_Ofertar
 {
@@ -14,6 +16,70 @@ namespace FrbaCommerce.Comprar_Ofertar
         public ComprarOfertar()
         {
             InitializeComponent();
+            cargarPublicaciones(0,50);
         }
+
+        public void cargarPublicaciones(int cant, int desde)
+        {
+            List<SqlParameter> listaParametros = new List<SqlParameter>();
+            BDSQL.agregarParametro(listaParametros, "@cant", cant);
+            BDSQL.agregarParametro(listaParametros, "@desde", desde);
+            /*
+            String commandtext = ("SELECT TOP (@cant) * " +
+            "FROM MERCADONEGRO.Publicaciones " +
+            "WHERE Cod_Publicacion NOT IN(Select TOP (@desde) Cod_Publicacion from MERCADONEGRO.Publicaciones) " +
+            "AND Estado_Publicacion = 0 " +
+            "AND Stock > 0 " +
+            "AND Fecha_Vencimiento < GETUTCDATE() " +
+            "ORDER BY Cod_Visibilidad" );
+            */
+            String commandtext = "WITH NumberedMyTable AS (SELECT Cod_Publicacion, Stock, ROW_NUMBER() OVER (ORDER BY Cod_Visibilidad) AS RowNumber " +
+            "FROM MERCADONEGRO.Publicaciones WHERE Estado_Publicacion = 0 AND Stock > 0 AND Fecha_Vencimiento < GETUTCDATE() ) SELECT Cod_Publicacion,Stock FROM NumberedMyTable " +
+            "WHERE RowNumber BETWEEN @cant AND @desde";
+            DataTable ret = BDSQL.obtenerDataTable(commandtext, "T", listaParametros);
+
+            dataGridView1.DataSource = ret;
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text != "")
+            {
+                int filtro = Convert.ToInt32(textBox3.Text);
+                cargarPublicaciones(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), filtro);
+            }
+            else cargarPublicaciones(Convert.ToInt32(textBox1.Text),Convert.ToInt32(textBox2.Text));
+        }
+
+
+        public void cargarPublicaciones(int desde, int hasta, int filtro)
+        {
+            List<SqlParameter> listaParametros = new List<SqlParameter>();
+            BDSQL.agregarParametro(listaParametros, "@desde", desde);
+            BDSQL.agregarParametro(listaParametros, "@hasta", hasta);
+            BDSQL.agregarParametro(listaParametros, "@filtro", filtro);
+
+            /*
+            String commandtext = ("SELECT TOP (@cant) * " +
+            "FROM MERCADONEGRO.Publicaciones " +
+            "WHERE Cod_Publicacion NOT IN(Select TOP (@desde) Cod_Publicacion from MERCADONEGRO.Publicaciones) " +
+            "AND Estado_Publicacion = 0 " +
+            "AND Stock > 0 " +
+            "AND Fecha_Vencimiento < GETUTCDATE() " +
+            "ORDER BY Cod_Visibilidad" );
+            */
+            String commandtext = "WITH NumberedMyTable AS (SELECT Cod_Publicacion, Stock, ROW_NUMBER() OVER (ORDER BY Cod_Visibilidad) AS RowNumber " +
+            "FROM MERCADONEGRO.Publicaciones WHERE Stock = @filtro AND Estado_Publicacion = 0 AND Stock > 0 AND Fecha_Vencimiento < GETUTCDATE() ) SELECT Cod_Publicacion,Stock FROM NumberedMyTable " +
+            "WHERE RowNumber BETWEEN @cant AND @desde";
+            DataTable ret = BDSQL.obtenerDataTable(commandtext, "T", listaParametros);
+
+            dataGridView1.DataSource = ret;
+
+
+        }
+
     }
+    
 }
