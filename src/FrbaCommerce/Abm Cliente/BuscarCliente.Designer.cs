3520dc8915352ec34fef6fa11731268227551300
alan <lastotaku@gmail.com>
2014-06-07 23:07:55 -0300

    Avanzando

--- src/FrbaCommerce/Abm Cliente/BuscarCliente.Designer.cs
+++ src/FrbaCommerce/Abm Cliente/BuscarCliente.Designer.cs
@@ -28,21 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgResultados = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgResultados)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.dgResultados);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(10, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(270, 200);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Clientes encontrados";
+            // 
+            // dgResultados
+            // 
+            this.dgResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgResultados.Location = new System.Drawing.Point(9, 20);
+            this.dgResultados.Name = "dgResultados";
+            this.dgResultados.Size = new System.Drawing.Size(251, 135);
+            this.dgResultados.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 30);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "<<";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 30);
+            this.button2.TabIndex = 13;
+            this.button2.Text = ">>";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // BuscarCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.ClientSize = new System.Drawing.Size(291, 220);
+            this.Controls.Add(this.groupBox1);
             this.Name = "BuscarCliente";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Modificar o eliminar cliente - MercadoNegro";
+            this.Text = "Buscar Clientes - MercadoNegro";
             this.Load += new System.EventHandler(this.BuscarCliente_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgResultados)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgResultados;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- src/FrbaCommerce/Abm Cliente/BuscarCliente.cs
+++ src/FrbaCommerce/Abm Cliente/BuscarCliente.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using FrbaCommerce.Common;
 
 namespace FrbaCommerce.Abm_Cliente
 {
@@ -13,6 +15,22 @@ namespace FrbaCommerce.Abm_Cliente
     {
         public char filtro { get; set; }
         public string valor { get; set; }
+        public int cantResultados { get; set; }
+
+        public class ResultadoClientes {
+            public int ID_User { get; set; }
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+
+            public ResultadoClientes(int _ID_User, string _Nombre, string _Apellido)
+            {
+                ID_User = _ID_User;
+                Nombre = _Nombre;
+                Apellido = _Apellido;
+            }
+        }
+
+        public List<ResultadoClientes> resultados = new List<ResultadoClientes>();
 
         public BuscarCliente(char _filtro, string _valor)
         {
@@ -20,9 +38,211 @@ namespace FrbaCommerce.Abm_Cliente
             this.valor = _valor;
 
             InitializeComponent();
+
+            this.cantResultados = cargarLista();
+            formatearDataGrid();
+        }
+
+        public int cargarLista()
+        {
+            int resultado = 0;
+
+            switch (filtro)
+            {
+                case 'N': // Nombre
+                    resultado = buscarNombre();
+                    break;
+                case 'A': // Apellido
+                    resultado = buscarApellido();
+                    break;
+                case 'T': // Tipo de documento
+                    resultado = buscarTipoDeDocumento();
+                    break;
+                case 'D': // Número de documento
+                    resultado = buscarNumeroDeDocumento();
+                    break;
+                case 'E': // E-mail
+                    resultado = buscarEmail();
+                    break;
+            }
+
+            return resultado;
+        }
+
+        public int buscarNombre()
+        {
+            List<SqlParameter> listaParametros = new List<SqlParameter>();
+            BDSQL.agregarParametro(listaParametros, "@Nombre", this.valor);
+            SqlDataReader lector = BDSQL.ejecutarReader("SELECT ID_User, Nombre, Apellido FROM MERCADONEGRO.Clientes WHERE Nombre = @Nombre", listaParametros, BDSQL.iniciarConexion());
+
+            int cantRes = 0;
+
+            if (lector.HasRows)
+            {
+                while (lector.Read())
+                {
+                    int ID_User = Convert.ToInt32(lector["ID_User"]);
+                    string Nombre = Convert.ToString(lector["Nombre"]);
+                    string Apellido = Convert.ToString(lector["Apellido"]);
+
+                    ResultadoClientes resultado = new ResultadoClientes(ID_User, Nombre, Apellido);
+                    resultados.Add(resultado);
+                }
+            }
+
+            BDSQL.cerrarConexion();
+            return cantRes;
+        }
+
+        public int buscarApellido()
+        {
+            List<SqlParameter> listaParametros = new List<SqlParameter>();
+            BDSQL.agregarParametro(listaParametros, "@Apellido", this.valor);
+            SqlDataReader lector = BDSQL.ejecutarReader("SELECT ID_User, Nombre, Apellido FROM MERCADONEGRO.Clientes WHERE Apellido = @Apellido", listaParametros, BDSQL.iniciarConexion());
+
+            int cantRes = 0;
+
+            if (lector.HasRows)
+            {
+                while (lector.Read())
+                {
+                    int ID_User = Convert.ToInt32(lector["ID_User"]);
+                    string Nombre = Convert.ToString(lector["Nombre"]);
+                    string Apellido = Convert.ToString(lector["Apellido"]);
+
+                    ResultadoClientes resultado = new ResultadoClientes(ID_User, Nombre, Apellido);
+                    resultados.Add(resultado);
+                }
+            }
+
+            BDSQL.cerrarConexion();
+            return cantRes;
+        }
+
+        public int buscarTipoDeDocumento()
+        {
+            List<SqlParameter> listaParametros = new List<SqlParameter>();
+            BDSQL.agregarParametro(listaParametros, "@Tipo_Doc", this.valor);
+            SqlDataReader lector = BDSQL.ejecutarReader("SELECT ID_User, Nombre, Apellido FROM MERCADONEGRO.Clientes WHERE Tipo_Doc = @Tipo_Doc", listaParametros, BDSQL.iniciarConexion());
+
+            int cantRes = 0;
+
+            if (lector.HasRows)
+            {
+                while (lector.Read())
+                {
+                    int ID_User = Convert.ToInt32(lector["ID_User"]);
+                    string Nombre = Convert.ToString(lector["Nombre"]);
+                    string Apellido = Convert.ToString(lector["Apellido"]);
+
+                    ResultadoClientes resultado = new ResultadoClientes(ID_User, Nombre, Apellido);
+                    resultados.Add(resultado);
+                }
+            }
+
+            BDSQL.cerrarConexion();
+            return cantRes;
+        }
+
+        public int buscarNumeroDeDocumento()
+        {
+            List<SqlParameter> listaParametros = new List<SqlParameter>();
+            BDSQL.agregarParametro(listaParametros, "@Num_Doc", this.valor);
+            SqlDataReader lector = BDSQL.ejecutarReader("SELECT ID_User, Nombre, Apellido FROM MERCADONEGRO.Clientes WHERE Num_Doc = @Num_Doc", listaParametros, BDSQL.iniciarConexion());
+
+            int cantRes = 0;
+
+            if (lector.HasRows)
+            {
+                while (lector.Read())
+                {
+                    int ID_User = Convert.ToInt32(lector["ID_User"]);
+                    string Nombre = Convert.ToString(lector["Nombre"]);
+                    string Apellido = Convert.ToString(lector["Apellido"]);
+
+                    ResultadoClientes resultado = new ResultadoClientes(ID_User, Nombre, Apellido);
+                    resultados.Add(resultado);
+                }
+            }
+
+            BDSQL.cerrarConexion();
+            return cantRes;
+        }
+
+        public int buscarEmail()
+        {
+            List<SqlParameter> listaParametros = new List<SqlParameter>();
+            BDSQL.agregarParametro(listaParametros, "@Mail", this.valor);
+            SqlDataReader lector = BDSQL.ejecutarReader("SELECT ID_User, Nombre, Apellido FROM MERCADONEGRO.Clientes WHERE Mail = @Mail", listaParametros, BDSQL.iniciarConexion());
+
+            int cantRes = 0;
+
+            if (lector.HasRows)
+            {
+                while (lector.Read())
+                {
+                    int ID_User = Convert.ToInt32(lector["ID_User"]);
+                    string Nombre = Convert.ToString(lector["Nombre"]);
+                    string Apellido = Convert.ToString(lector["Apellido"]);
+
+                    ResultadoClientes resultado = new ResultadoClientes(ID_User, Nombre, Apellido);
+                    resultados.Add(resultado);
+                }
+            }
+
+            BDSQL.cerrarConexion();
+            return cantRes;
+        }
+
+        public void formatearDataGrid()
+        {
+            int widthNombre = 100;
+            int widthApellido = 100;
+            int widthBotones = 100;
+
+            dgResultados.DataSource = resultados;
+            dgResultados.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgResultados.RowHeadersVisible = false;
+
+            DataGridViewColumn col_idUser = dgResultados.Columns[0];
+            col_idUser.Visible = false;
+
+            DataGridViewColumn col_nombre = dgResultados.Columns[1];
+            col_nombre.Resizable = DataGridViewTriState.False;
+            col_nombre.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            col_nombre.Width = widthNombre;
+
+            DataGridViewColumn col_apellido = dgResultados.Columns[1];
+            col_apellido.Resizable = DataGridViewTriState.False;
+            col_apellido.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            col_apellido.Width = widthApellido;
+
+            DataGridViewButtonColumn botonesModificar = new DataGridViewButtonColumn();
+            botonesModificar.HeaderText = "";
+            botonesModificar.Text = "Modificar";
+            botonesModificar.Name = "bMod";
+            botonesModificar.Width = widthBotones;
+            botonesModificar.UseColumnTextForButtonValue = true;
+            botonesModificar.Resizable = DataGridViewTriState.False;
+
+            DataGridViewButtonColumn botonesEliminar = new DataGridViewButtonColumn();
+            botonesEliminar.HeaderText = "";
+            botonesEliminar.Text = "Eliminar";
+            botonesEliminar.Name = "bElim";
+            botonesEliminar.Width = widthBotones;
+            botonesEliminar.UseColumnTextForButtonValue = true;
+            botonesEliminar.Resizable = DataGridViewTriState.False;
+
+            dgResultados.Columns.Add(botonesModificar);
+            dgResultados.Columns.Add(botonesEliminar);
         }
 
         private void BuscarCliente_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
