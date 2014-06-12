13cc55a5e1c6ffcf47aea47916e2e05e040e31d4
NiL-22 <nazareno.levy@gmail.com>
2014-06-12 19:54:33 -0300

    Importados los SP de Alan

--- data/script_inicial_MN_beta.sql
+++ data/script_inicial_MN_beta.sql
@@ -469,7 +469,48 @@ DEALLOCATE trigger_cursor;
 END
 GO
 
+CREATE PROCEDURE MERCADONEGRO.obtenerCompras
+	@ID_User NUMERIC(18,0)
+AS
+	SELECT ID_Vendedor, Cod_Publicacion, Cod_Calificacion, Fecha_Operacion FROM MERCADONEGRO.Operaciones WHERE ID_Comprador = @ID_User AND Cod_TipoOperacion = 0
+GO
 
+CREATE PROCEDURE MERCADONEGRO.obtenerOfertasGanadas
+	@ID_User NUMERIC(18,0)
+AS
+	SELECT ID_Vendedor, Cod_Publicacion, Cod_Calificacion, Fecha_Operacion FROM MERCADONEGRO.Operaciones WHERE ID_Comprador = @ID_User AND Cod_TipoOperacion = 1
+GO
+
+CREATE PROCEDURE MERCADONEGRO.obtenerOfertas
+	@ID_User NUMERIC(18,0)
+AS
+	SELECT ID_Vendedor, Cod_Publicacion, Fecha_Oferta, Monto_Oferta FROM MERCADONEGRO.Subastas WHERE ID_Comprador = @ID_User
+GO
+
+CREATE PROCEDURE MERCADONEGRO.eliminarRubro
+	@ID_Rubro NUMERIC(18,0)
+AS
+	UPDATE MERCADONEGRO.Rubro_Publicacion SET ID_Rubro = 1 WHERE ID_Rubro = @ID_Rubro
+	DELETE FROM MERCADONEGRO.Rubros WHERE ID_Rubro = @ID_Rubro
+GO
+
+CREATE PROCEDURE MERCADONEGRO.obtenerVendedor
+	@ID_Vendedor NUMERIC(18,0)
+AS
+	SELECT Username FROM MERCADONEGRO.Usuarios WHERE ID_User = @ID_Vendedor
+GO
+
+CREATE PROCEDURE MERCADONEGRO.obtenerPublicacion
+	@Cod_Publicacion NUMERIC(18,0)
+AS
+	SELECT Descripcion FROM MERCADONEGRO.Publicaciones WHERE Cod_Publicacion = @Cod_Publicacion
+GO
+
+CREATE PROCEDURE MERCADONEGRO.obtenerCalificacion
+	@Cod_Calificacion NUMERIC(18,0)
+AS
+	SELECT Puntaje, Descripcion FROM MERCADONEGRO.Calificaciones WHERE Cod_Calificacion = @Cod_Calificacion
+GO
 
 /*
 CREATE PROCEDURE MERCADONEGRO.InsertarCliente(@tipoDoc nvarchar(50),
